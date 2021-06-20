6629bb5fead048632f412c8df8769645283f44be
Alaa Jamal <alaasaqllah@gmail.com>
2021-06-20 10:43:54 +0300

    new file login added

--- student.py
+++ student.py
@@ -10,3 +10,12 @@ my_function("Sweden")
 my_function("India")
 my_function()
 my_function("Brazil")
+
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
